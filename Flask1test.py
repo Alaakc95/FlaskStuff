3c8b6b25abf3a8522704a54ca84593fe61ad4817
Alaakc <Akc95@live.com>
2022-03-03 20:41:06 +0100

    Second Flask test commit

--- Flask1test.py
+++ Flask1test.py
@@ -1,14 +1,51 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return 'Hello, world!'
+#@app.route('/')
+#def homepage():
+#    return 'Hello, world!'
 
-@app.route('/index')
-def indexpage():
-    return 'This is the index page :)'
+#@app.route('/<name>')
+#def indexpage(name):
+#    return 'This is the ' +name+' page'
+
+#@app.route('/blog/<int:postID>')
+#def show_blog(postID):
+#    return 'Blog Number %d' % postID
+
+#@app.route('/rev/<float:revNo>')
+#def revesion(revNo):
+#return 'Revision Number %f' % revNo
+
+#@app.route('/admin')
+#def hello_admin():
+#    return 'Hello Admin'
+
+#@app.route('/guest/<guest>')
+#def hello_guest(guest):
+#    return 'Hello %s as Guest' % guest
+
+#@app.route('/user/<name>')
+#def hello_user(name):
+#    if name == 'admin':
+#        return redirect(url_for('hello_admin'))
+#    else:
+#        return redirect(url_for('hello_guest', guest = name))
+
+###### HTTP METHODS #####
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' %name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name = user))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
