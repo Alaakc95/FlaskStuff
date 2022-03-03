f6714108b6a715e888919cff5b186c370dc168a0
Alaakc <Akc95@live.com>
2022-03-03 22:03:22 +0100

    update Flask test commit

--- Flask1test.py
+++ Flask1test.py
@@ -49,9 +49,22 @@ app = Flask(__name__)
 
 ####### Flask - Templates
 
-@app.route('/hello/<user>')
-def hello_name(user):
-    return render_template('hello.html', name = user)
+#@app.route('/hello/<user>')
+#def hello_name(user):
+#    return render_template('hello.html', name = user)
+
+#@app.route('/conditional/<int:score>')
+#def hello_name(score):
+#    return render_template('conditional.html', marks = score)
+
+#@app.route('/result')
+#def result():
+#    dict = {'phy':50,'che':60,'maths':70}
+#    return render_template('result.html', result = dict)
+
+############ Request Object
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
